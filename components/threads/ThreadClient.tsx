eaf2a1ac00fb074ee30c8728294f7e45e36ddc13
tama235 <al25022@shibaura-it.ac.jp>
2026-02-28 21:03:15 +0900

    ちょっと修正

--- components/threads/ThreadClient.tsx
+++ components/threads/ThreadClient.tsx
@@ -4,7 +4,6 @@ import { MessageComposer } from "@/components/threads/MessageComposer";
 import { MessageList } from "@/components/threads/MessageList";
 import { ThreadHeader } from "@/components/threads/ThreadHeader";
 import { useEffect, useMemo, useState } from "react";
-import { mockThreads } from "@/mock/threads";
 import { Message } from "@/types/message";
 import { supabase } from "@/lib/supabaseClient";
 
@@ -14,11 +13,11 @@ type Props = {
 
 export default function ThreadClient({ threadId }: Props) {
   const [messages, setMessages] = useState<Message[]>([]);
-  const thread = mockThreads.find((t) => t.id === threadId);
-  const mediatorType = thread?.mediatorType ?? "plush";
+  const mediatorType: "plush" | "plant" | "ojisan" = "plush";
+  const title = "家事の分担"; // ここも一旦固定でOK
 
   // 今はログイン無しなので固定（usersテーブルのAのid）
-  const currentUserId = "a3db4705-3c8f-4b4d-aae0-09500e4dc44e";
+  const currentUserId = "cd3a3a03-d0de-40b0-96c7-a0e34cbb9ed7";
 
   // --- DBからこのスレのメッセージを取得 ---
   async function fetchMessages() {
@@ -37,6 +36,7 @@ export default function ThreadClient({ threadId }: Props) {
   }
 
   useEffect(() => {
+    if (!threadId) return;
     fetchMessages();
   }, [threadId]);
 
@@ -92,12 +92,13 @@ export default function ThreadClient({ threadId }: Props) {
         new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime(),
     );
   }, [messages]);
+  console.log("✅ ThreadClient rendered. threadId =", threadId);
 
   return (
     <div className="flex flex-col min-h-screen bg-[#E5E3D6] bg-[url(/rectangle23.svg)] bg-contain bg-no-repeat bg-top ">
       <ThreadHeader
         threadId={threadId}
-        title={thread?.body}
+        title={title}
         onRefresh={fetchMessages}
       />
       <MessageList
