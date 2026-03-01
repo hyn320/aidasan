7722833373e47e01b431730541ef1bbbc2d29d86
tama235 <al25022@shibaura-it.ac.jp>
2026-03-01 12:15:55 +0900

    完成

---
+++ lib/devUser.ts
@@ -0,0 +1,4 @@
+// src/lib/devUser.ts
+
+export const CURRENT_USER_ID = "a3db4705-3c8f-4b4d-aae0-09500e4dc44e"; // ← A固定
+// export const CURRENT_USER_ID = "cd3a3a03-d0de-40b0-96c7-a0e34cbb9ed7"; // ← B固定
